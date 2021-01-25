31d2b2c2e93d3f22462de2932455abfda69d9ea1
dvrarunkumar <74497383+dvrarunkumar@users.noreply.github.com>
2021-01-25 19:29:58 +0530

    First upload from desktop to GIT url location

---
+++ src/main/java/com/akd/main/AKDSpringBootStarter.java
@@ -0,0 +1,11 @@
+package com.akd.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AKDSpringBootStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(AKDSpringBootStarter.class, args);
+    }
+}
