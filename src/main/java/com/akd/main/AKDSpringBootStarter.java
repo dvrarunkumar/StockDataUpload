d24ef2902ce141d1d0f4b03931df4c7f4e7aba5b
dvrarunkumar <74497383+dvrarunkumar@users.noreply.github.com>
2021-01-25 19:46:55 +0530

    kkk0

--- src/main/java/com/akd/main/AKDSpringBootStarter.java
+++ src/main/java/com/akd/main/AKDSpringBootStarter.java
@@ -2,7 +2,7 @@ package com.akd.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// akd
 @SpringBootApplication
 public class AKDSpringBootStarter {
     public static void main(String[] args) {
